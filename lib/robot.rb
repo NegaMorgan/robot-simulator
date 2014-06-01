0b0aa87f502812c87f0a0366557a671bbac6b314
Morgan Evans <morganrevans@gmail.com>
2014-06-01 10:13:27 -0400

    First draft complete

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,18 +1,53 @@
 class Robot
   attr_reader :bearing, :coordinates
   VALID_DIRECTIONS = ['north','east','south','west']
+  VALID_ACTIONS = {:L => 'turn_left', :R => 'turn_right', :A => 'advance'}
 
   def orient(direction)
     raise "Invalid Robot Bearing" unless VALID_DIRECTIONS.include? direction
     @bearing = direction
   end
+  def turn(number)
+    orient(VALID_DIRECTIONS[(VALID_DIRECTIONS.index(bearing)+number)%4])
+  end
   def turn_right
-    orient(VALID_DIRECTIONS[(VALID_DIRECTIONS.index(bearing)+1)%4])
+    turn(1)
   end
   def turn_left
-    orient(VALID_DIRECTIONS[(VALID_DIRECTIONS.index(bearing)+3)%4])
+    turn(3)
   end
   def at(x, y)
     @coordinates = [x, y]
+  end
+  def advance
+    x = coordinates[0]
+    y = coordinates[1]
+
+    case bearing
+    when 'north'
+      y += 1
+    when 'east'
+      x += 1
+    when 'south'
+      y -= 1
+    when 'west'
+      x -= 1
+    end
+
+    self.at(x, y)
+  end
+  def instructions(letters)
+    letters.split("").map do |command| 
+      VALID_ACTIONS[command.to_sym]
+    end
+  end
+  def place(options)
+    at options[:x], options[:y]
+    orient options[:direction]
+  end
+  def evaluate(letters)
+    letters.split("").each do |command| 
+      self.send(VALID_ACTIONS[command.to_sym])
+    end
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -70,10 +70,80 @@ describe 'Robot' do
     @robot.at(3,0)
     expect(@robot.coordinates).to eq [3,0]
   end
-
   it "other robot coordinates" do 
     @robot.at(-2, 5)
     expect(@robot.coordinates).to eq [-2, 5]
+  end
+
+  it "advance when facing north" do
+    @robot.at(0,0)
+    @robot.orient('north')
+    @robot.advance
+
+    expect(@robot.coordinates).to eq [0,1]
+  end
+  it "advance when facing east" do
+    @robot.at(0,0)
+    @robot.orient('east')
+    @robot.advance
+
+    expect(@robot.coordinates).to eq [1,0]
+  end
+  it "advance when facing south" do
+    @robot.at(0,0)
+    @robot.orient('south')
+    @robot.advance
+
+    expect(@robot.coordinates).to eq [0,-1]
+  end
+  it "advance when facing west" do
+    @robot.at(0,0)
+    @robot.orient('west')
+    @robot.advance
+
+    expect(@robot.coordinates).to eq [-1,0]
+  end
+
+  it "instructions for turning left" do 
+    expect(@robot.instructions('L')).to eq ["turn_left"]
+  end
+  it "instructions for turning right" do 
+    expect(@robot.instructions('R')).to eq ["turn_right"]
+  end
+  it "instructions for advancing" do 
+    expect(@robot.instructions('A')).to eq ["advance"]
+  end
+  it "series of instructions" do 
+    expect(@robot.instructions('RAAL')).to eq ["turn_right", "advance", "advance", "turn_left"]
+  end
+
+  it "instruct robot" do 
+    @robot.place({x: -2, y: 1, direction: "east"})
+    @robot.evaluate 'RLAALAL'
+
+    expect(@robot.coordinates).to eq [0,2]
+    expect(@robot.bearing).to eq 'west'
+  end
+  it "instruct many robots" do 
+    robot1 = Robot.new
+    robot2 = Robot.new
+    robot3 = Robot.new
+    
+    robot1.place({x: 0, y: 0, direction: "north"})
+    robot2.place({x: 2, y: -7, direction: "east"})
+    robot3.place({x: 8, y: 4, direction: "south"})
+    robot1.evaluate("LAAARALA")
+    robot2.evaluate("RRAAAAALA")
+    robot3.evaluate("LAAARRRALLLL")
+
+    expect(robot1.coordinates).to eq [-4, 1]
+    expect(robot1.bearing).to eq "west"
+
+    expect(robot2.coordinates).to eq [-3, -8]
+    expect(robot2.bearing).to eq "south"
+
+    expect(robot3.coordinates).to eq [11, 5]
+    expect(robot3.bearing).to eq "north"
   end
   
 end
